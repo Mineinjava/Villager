faeb681ef730cf8ba6f2d6eaaa6eeddd27f0b209
Mineinjava <fbisurveillancevan525178@gmail.com>
2021-01-08 13:50:52 -0800

    added .gitignore
    things happened

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import discord
 import json
 import random
 from discord.ext import commands
-
+import os
 
 client = commands.Bot(command_prefix='%')
 # note: I never use the commands 
@@ -13,8 +13,10 @@ chatbot = ChatBot('Warden')
 
 # train ML
 from chatterbot.trainers import ChatterBotCorpusTrainer
+
 trainer = ChatterBotCorpusTrainer(chatbot)
 from chatterbot.trainers import ListTrainer
+
 trainer.train(
     "chatterbot.corpus.english.botprofile",
     "chatterbot.corpus.english.conversations",
@@ -24,29 +26,30 @@ listtrainer = ListTrainer(chatbot)
 # make it so
 convo = ["hello there", "hi"]
 
-#with open("convo.pkl", "wb") as fp:   #Pickling
-    #pickle.dump(convo, fp)
+# with open("convo.pkl", "wb") as fp:   #Pickling
+# pickle.dump(convo, fp)
 
 try:
-    with open('convo.pkl', 'rb') as fp:   # Unpickling
+    with open('convo.pkl', 'rb') as fp:  # Unpicklingdir
         convo = pickle.load(fp)
 except Exception:
-    with open("convo.pkl", "wb") as fp:   #Pickling
+    with open("convo.pkl", "wb") as fp:  # Pickling
         pickle.dump(convo, fp)
 
 silenceMode = False
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user} (chatbot mode)'.format(client))
 
+
 @client.event
 async def on_message(message):
-
     if message.author == client.user:
         return
 
-    if '%' in message.content: # change to bot prefix
+    if '%' in message.content:  # change to bot prefix
         return
 
     if "http" in message.content:
@@ -54,22 +57,17 @@ async def on_message(message):
 
     contnt = message.content
     if silenceMode == False:
-        if message.channel.id == 746774706350456913 or message.channel.id == 768196564677296138 or message.channel.id == 768196564677296138:
-            shouldRespond = random.randint(1, 5)
-            if shouldRespond == 1:
-                return
-            response = chatbot.get_response(contnt)
-            print(response)
-            await message.channel.send(response)
+        shouldRespond = random.randint(1, 5)
+        if shouldRespond == 1:
+            return
+        response = chatbot.get_response(contnt)
+        print(response)
+        await message.channel.send(response)
 
     if message.author.bot:
-        print("cannot learn from a bot")
         return
-
-    if message.channel.id == 696124006822314061:
+    else:
         convo.append(message.content)
-
-
 
 
 @client.command()
@@ -83,8 +81,10 @@ async def learn(ctx):
     listtrainer.train(
         convo
     )
-    with open("convo.pkl", "wb") as fp:   #Pickling
-           pickle.dump(convo, fp)
+    with open("convo.pkl", "wb") as fp:  # Pickling
+        pickle.dump(convo, fp)
     await ctx.send("recalibrated AI")
-        
-client.run('')
+
+
+token = os.environ['TOKEN']
+client.run(token)
